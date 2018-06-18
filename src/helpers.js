9eef06264500091fa27bca1e510f0095cdea8844
unknown <mattwong789@gmail.com>
2018-06-17 17:03:52 -0700

    new files

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+
+/*
+export function getCategories(pid) {
+    
+} 
+router.get('/:pid', controller.getCategories);
+
+export function rando(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+*/
